ae144337c0c664e3e55daec796ed3deac15bbb01
luisruiz3012 <luisdiegoaguilar@outlook.com>
2022-04-20 08:32:21 -0600

    sending to github

---
+++ Hola.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, excepturi illum dignissimos velit suscipit, aspernatur dolor, ipsa nostrum alias iusto beatae reiciendis ducimus officiis dolores repudiandae repellendus at recusandae natus!</p>
+        <p>{{text}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: "Hola vue",
+        }
+    }
+}
+</script>
+
+<style scoped>
+    p {
+        color: red;
+    }
+</style>
